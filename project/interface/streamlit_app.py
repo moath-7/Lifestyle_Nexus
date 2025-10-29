805429d2daf3699931bb73c805f23a0ccabbd795
Moath Alghamdi <Moathatyic@gmail.com>
2025-10-29 12:04:18 +0300

    back to working design

--- project/interface/streamlit_app.py
+++ project/interface/streamlit_app.py
@@ -163,22 +163,22 @@ def form_page():
 def results_page():
     st.markdown("<h2 style='text-align:center;'>Your Health Prediction Result</h2>", unsafe_allow_html=True)
 
-    if 'prediction' in st.session_state and 'user_data' in st.session_state:
-        prediction, prob = st.session_state['prediction']
-        user_data = st.session_state['user_data']
+    if 'prediction' in st.session_state:
+        prediction, prob = st.session_state['prediction']["prediction"], st.session_state['prediction']["prob"]
+
         # بطاقة النتيجة
         if prediction == 1:
             st.markdown(f"""
                 <div style='background-color:#D4EDDA; padding:25px; border-radius:15px; text-align:center;'>
                     <h3 style='color:#155724;'>✅ Your lifestyle is healthy with a probability of</h3>
-                    <p style='font-size:18px; color:#155724;'>Your health Probability: <b>{prob:.1f}%</b> <br>Keep up the good work maintaining a healthy lifestyle.</p>
+                    <p style='font-size:18px; color:#155724;'>Your health Probability: <b>{prob:.1f}%</b> Keep up the good work maintaining a healthy lifestyle.</p>
                 </div>
             """, unsafe_allow_html=True)
         else:
             st.markdown(f"""
                 <div style='background-color:#F8D7DA; padding:25px; border-radius:15px; text-align:center;'>
                     <h3 style='color:#721C24;'>⚠️ Your lifestyle is unhealthy </h3>
-                    <p style <p style='font-size:18px; color:#721C24;'> Your health Probability: <b>{prob:.1f}%</b> <br>and you are at risk of lifestyle diseases. Consider making healthier choices.</p>
+                    <p style <p style='font-size:18px; color:#721C24;'> Your health Probability: <b>{100-prob:.1f}%</b>and you are at risk of lifestyle diseases. Consider making healthier choices.</p>
                 </div>
             """, unsafe_allow_html=True)
 
